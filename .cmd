4a8a2603f1b561602f2f2f0c7f12980ac97b1b55
Gopala-kannan <165189430+Gopala-kannan@users.noreply.github.com>
2025-06-04 11:04:56 +0530

    Create .cmd

---
+++ .cmd
@@ -0,0 +1,24 @@
+youtube cmd code
+
+> pip install packages
+> python -m venv myapp
+> myapp\Scripts\activate
+> python -m pip install Django
+> django-admin --version
+> django-admin startproject my_app .
+> python .\manage.py runserver
+> python .\manage.py runserver startapp blog
+
+
+inmakes learninghub terminal code
+
+> pip install packages
+> python -m venv foldername
+> cd foldername
+> cd Scripts
+> activate
+> cd .. => its use to come back of the folder
+> pip install django
+> django-admin
+> djangp-admin startproject project_name .
+> python .\manage.py runserver => its used to run server in web
